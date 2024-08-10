53238be837a5f64c6c94902976dd4fe50d62fe33
Geeth-Rath <geeth4862@gmail.com>
2024-08-10 11:05:00 +0530

    user registration done

--- app/main.py
+++ app/main.py
@@ -1,9 +1,38 @@
-from fastapi import FastAPI, APIRouter, status
+from fastapi import FastAPI, APIRouter, status, Depends, HTTPException
+from sqlalchemy.orm import Session
+
+from app.databse.database import get_db
+from app.schemas.schemas import UserCreate
+from app.service import service
+from app.databse.database import Base, engine
 
 app = FastAPI()
-router = APIRouter()
+router = APIRouter(prefix="/users", tags=["Users"])
+
+# Create all tables
+Base.metadata.create_all(bind=engine)
+
 
 @app.get("/api/healthchecker")
 def root():
     return {"message": "Welcome to FastAPI with SQLAlchemy"}
 
+
+@router.post("/register", status_code=status.HTTP_201_CREATED)
+async def create_user(user: UserCreate, db: Session = Depends (get_db)):
+    db_user = await service.existing_user(db, user.username, user.email)
+    if db_user:
+        raise HTTPException(
+            status_code=status.HTTP_409_CONFLICT,
+            detail="username or email already in use",
+        )
+    db_user = await service.create_user(db, user)
+    access_token = await service.create_access_token (db_user.id, db_user.username)
+
+    return {
+        "access_token": access_token,
+        "token_type":"bearer",
+        "username": db_user.username,
+    }
+
+app.include_router(router)
--- app/service/service.py
+++ app/service/service.py
@@ -1,9 +1,23 @@
+from datetime import timedelta, datetime
+
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer
 from sqlalchemy.orm import Session
 from app.model.model import User as UserModel
+from jose import jwt, JWTError
+from passlib.context import CryptContext
 
+from app.schemas.schemas import UserCreate
+
+SECRET_KEY = "mysecretkey"
+EXPIRE_MINUTES = 60 * 20
+ALGORITHUM = "HS256"
+
+oauth2_bearer = OAuth2PasswordBearer(tokenUrl = "token")
+bcrypt_context = CryptContext(schemes = ["bcrypt"])
 
 # check existing users with username and email
-async def exixting_user(db: Session, username: str, email:str):
+async def existing_user(db: Session, username: str, email:str):
     db_user = db.query(UserModel).filter(UserModel.username == username).first()
     if db_user:
         return db_user
@@ -13,3 +27,40 @@ async def exixting_user(db: Session, username: str, email:str):
     return None
 
 
+# create token
+#jwt = {encoded data, secret key, algorithm}
+async def create_access_token (id: int, username: str):
+    encode = {"sub": username, "id": id}
+    expires= datetime.utcnow() + timedelta (minutes=EXPIRE_MINUTES)
+    encode.update({"exp": expires})
+    return jwt.encode(encode, SECRET_KEY, algorithm=ALGORITHUM)
+
+
+# get current user from token
+async def get_current_user(db: Session, token: str = Depends (oauth2_bearer)):
+    try:
+        payload=jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHUM])
+        username: str = payload.get("sub")
+        id: int = payload.get("id")
+        expires: datetime = payload.get("exp")
+        if expires < datetime.utcnow():
+            return None
+        if username is None or id is None:
+            return None
+        db_user = db.query(UserModel).filter(UserModel.id == id).first()
+        return db_user
+    except JWTError:
+        return None
+
+
+
+#create user
+async def create_user(db: Session, user: UserCreate):
+    db_user = UserModel(
+            username=user.username,
+            email=user.email,
+            hashed_password=bcrypt_context.hash(user.password),
+    )
+    db.add(db_user)
+    db.commit()
+    return db_user
